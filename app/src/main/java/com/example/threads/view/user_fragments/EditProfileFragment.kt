0a1685b360d70f1361457135a1068f67c6993e7a
Vlad <tsoiivv@gmail.com>
2023-08-17 11:48:56 +0600

    T1-140, added some ui changes and circle images

--- app/src/main/java/com/example/threads/view/user_fragments/EditProfileFragment.kt
+++ app/src/main/java/com/example/threads/view/user_fragments/EditProfileFragment.kt
@@ -1,18 +1,25 @@
 package com.example.threads.view.user_fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.threads.MainActivity
 import com.example.threads.R
 import com.example.threads.databinding.FragmentEditProfileBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.imageview.ShapeableImageView
 
 class EditProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentEditProfileBinding
+    private lateinit var circularImageView: ShapeableImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,11 +34,59 @@ class EditProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity() as MainActivity).hide()
         navigation()
+        imageInstall()
+    }
+
+    private fun imageInstall() {
+        circularImageView = binding.imgSelectImageUser
+
+        val changePhotoTextView = binding.txtChangePhoto
+        changePhotoTextView.setOnClickListener {
+            showBottomSheetDialog()
+        }
     }
 
     private fun navigation() {
         binding.btnCancelEdit.setOnClickListener {
             findNavController().navigate(R.id.action_editProfileFragment_to_userMainFragment)
+        }
+    }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetDialog = BottomSheetDialog(requireContext())
+        val bottomSheetView = layoutInflater.inflate(R.layout.btm_dialog_add_photo, null)
+
+        val addPhotoButton = bottomSheetView.findViewById<TextView>(R.id.btn_addPhoto)
+        val removePhotoButton = bottomSheetView.findViewById<TextView>(R.id.btn_removePhoto)
+
+        addPhotoButton.setOnClickListener {
+            openGallery()
+            bottomSheetDialog.dismiss()
+        }
+
+        removePhotoButton.setOnClickListener {
+            circularImageView.setImageResource(R.drawable.img_userphoto)
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.show()
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    companion object {
+        private const val GALLERY_REQUEST_CODE = 123
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val imageUri = data?.data
+            circularImageView.setImageURI(imageUri)
         }
     }
 }
--- app/src/main/java/com/example/threads/view/user_fragments/UserMainFragment.kt
+++ app/src/main/java/com/example/threads/view/user_fragments/UserMainFragment.kt
@@ -4,19 +4,27 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.threads.MainActivity
 import com.example.threads.R
 import com.example.threads.databinding.FragmentUserMainBinding
+import com.example.threads.models.FeedItem
+import com.example.threads.utils.FeedAdapter
 import kotlinx.android.synthetic.main.custom_dialog_logout.view.*
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
 
 class UserMainFragment : Fragment() {
 
     private lateinit var binding: FragmentUserMainBinding
+    private lateinit var feedAdapter: FeedAdapter
+    private lateinit var recyclerView: RecyclerView
+    private val feedItems: MutableList<FeedItem> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,11 +41,31 @@ class UserMainFragment : Fragment() {
 
         logOut()
         navigation()
+        setupRV()
+    }
+
+    private fun setupRV() {
+
+        val testItems = listOf(
+            FeedItem(1, "User1", "This is the first thread."),
+            FeedItem(2, "User2", "Testing the second thread."),
+            FeedItem(3, "User3", "Third thread for testing."),
+            // Add more test items as needed
+        )
+
+        recyclerView = binding.rcFeedUserPage
+        feedAdapter = FeedAdapter(testItems)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = feedAdapter
     }
 
     private fun navigation() {
         binding.btnEditProfile.setOnClickListener {
             findNavController().navigate(R.id.action_userMainFragment_to_editProfileFragment)
+        }
+
+        binding.txtNumbFollowers.setOnClickListener {
+            findNavController().navigate(R.id.action_userMainFragment_to_tabFragment)
         }
     }
 
