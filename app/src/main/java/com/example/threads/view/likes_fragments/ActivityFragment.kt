0335a8b1401f5cf27806daed4625ccffecd470ef
Vlad <tsoiivv@gmail.com>
2023-08-30 17:04:22 +0600

    T1-225 added custom viewPager2 with button change state depending on fragment location

--- app/src/main/java/com/example/threads/view/likes_fragments/ActivityFragment.kt
+++ app/src/main/java/com/example/threads/view/likes_fragments/ActivityFragment.kt
@@ -1,13 +1,15 @@
 package com.example.threads.view.likes_fragments
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
-import androidx.core.widget.TextViewCompat
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.threads.R
 import com.example.threads.databinding.FragmentActivityBinding
@@ -16,6 +18,10 @@ import com.example.threads.utils.ActivityFragmentAdapter
 class ActivityFragment : Fragment() {
 
     private lateinit var binding: FragmentActivityBinding
+
+    private lateinit var button1: AppCompatButton
+    private lateinit var button2: AppCompatButton
+    private lateinit var button3: AppCompatButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,44 +44,36 @@ class ActivityFragment : Fragment() {
         val adapter = ActivityFragmentAdapter(requireActivity())
         viewPager.adapter = adapter
 
-        binding.btnActivityComments.setOnClickListener {
-            viewPager.currentItem = 0
-            updateButtonStates(0)
-        }
+        button1 = binding.btnActivityComments
+        button2 = binding.btnActivityFollowing
+        button3 = binding.btnActivityRequests
 
-        binding.btnActivityRequests.setOnClickListener {
-            viewPager.currentItem = 1
-            updateButtonStates(1)
-        }
+        setActiveButton(button1, viewPager, 0)
 
-        binding.btnActivityFollowing.setOnClickListener {
-            viewPager.currentItem = 2
-            updateButtonStates(2)
-        }
+        button1.setOnClickListener { setActiveButton(button1, viewPager, 0) }
+        button3.setOnClickListener { setActiveButton(button3, viewPager, 1) }
+        button2.setOnClickListener { setActiveButton(button2, viewPager, 2) }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> setActiveButton(button1, viewPager, 0)
+                    1 -> setActiveButton(button3, viewPager, 1)
+                    2 -> setActiveButton(button2, viewPager, 2)
+                }
+            }
+        })
     }
 
-    private fun updateButtonStates(position: Int) {
-        val btnComments = binding.btnActivityComments
-        val btnRequests = binding.btnActivityRequests
-        val btnFollowing = binding.btnActivityFollowing
-        
-        val currentPage = "comments";
+    private fun setActiveButton(activeButton: Button, viewPager: ViewPager2, position: Int) {
+        button1.setBackgroundResource(if (activeButton == button1) R.drawable.btn_rounded else R.drawable.btn_rounded_user_page)
+        button2.setBackgroundResource(if (activeButton == button2) R.drawable.btn_rounded else R.drawable.btn_rounded_user_page)
+        button3.setBackgroundResource(if (activeButton == button3) R.drawable.btn_rounded else R.drawable.btn_rounded_user_page)
 
-        if ("comments".equals(currentPage)) {
-            btnComments.setBackgroundResource(R.drawable.btn_google);
-            btnComments.setTextColor(getResources().getColor(R.color.black));
-            btnRequests.setBackgroundResource(R.drawable.btn_rounded);
-            btnFollowing.setBackgroundResource(R.drawable.btn_rounded);
-        } else if ("requests".equals(currentPage)) {
-            btnComments.setBackgroundResource(R.drawable.btn_rounded);
-            btnRequests.setBackgroundResource(R.drawable.btn_google);
-            btnRequests.setTextColor(getResources().getColor(R.color.black));
-            btnFollowing.setBackgroundResource(R.drawable.btn_rounded);
-        } else if ("following".equals(currentPage)) {
-            btnComments.setBackgroundResource(R.drawable.btn_rounded);
-            btnRequests.setBackgroundResource(R.drawable.btn_rounded);
-            btnFollowing.setBackgroundResource(R.drawable.btn_google);
-            btnFollowing.setTextColor(getResources().getColor(R.color.black));
-        }
+        button1.setTextColor(if (activeButton == button1) Color.WHITE else Color.BLACK)
+        button2.setTextColor(if (activeButton == button2) Color.WHITE else Color.BLACK)
+        button3.setTextColor(if (activeButton == button3) Color.WHITE else Color.BLACK)
+
+        viewPager.setCurrentItem(position, true)
     }
 }
