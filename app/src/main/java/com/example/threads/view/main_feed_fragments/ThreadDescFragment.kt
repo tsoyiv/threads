bcb2c54401cd6bca8b7c525c5aa245add0785b46
Vlad <tsoiivv@gmail.com>
2023-09-13 21:32:36 +0600

    T1-30 added thread desc page layout

---
+++ app/src/main/java/com/example/threads/view/main_feed_fragments/ThreadDescFragment.kt
@@ -0,0 +1,26 @@
+package com.example.threads.view.main_feed_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.threads.R
+import com.example.threads.databinding.FragmentThreadDescBinding
+
+class ThreadDescFragment : Fragment() {
+
+    private lateinit var binding: FragmentThreadDescBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentThreadDescBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
