55b69275bbb8185cf82583f5926c00f87f3745a0
Vlad <tsoiivv@gmail.com>
2023-09-13 22:58:05 +0600

    T1-12 first implementation of logic to adjust image and word counter

--- app/src/main/java/com/example/threads/view/creation_fragments/CreationThreadFragment.kt
+++ app/src/main/java/com/example/threads/view/creation_fragments/CreationThreadFragment.kt
@@ -60,6 +60,31 @@ class CreationThreadFragment : Fragment(), BottomMenuDialog.OnOptionSelectedList
         btnPostEnableCheck()
         createThread()
         isThreadAddedSuccess()
+        lengthChecker()
+    }
+
+    private fun lengthChecker() {
+        val item_ownerThread = binding.itemOwnerThread
+        val txtPost = binding.txtPost
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val textLength = s?.length ?: 0
+                if (textLength > 250) {
+                    txtPost.isEnabled = false
+                    Toast.makeText(requireContext(), "The Thread must be shorter than 250 symbols", Toast.LENGTH_SHORT).show()
+                } else {
+                    txtPost.isEnabled = true
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+
+        item_ownerThread.addTextChangedListener(textWatcher)
     }
 
     private fun isThreadAddedSuccess() {
